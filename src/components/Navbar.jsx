546537e3c9cd910ccd465c101f904faf85ed2859
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-03 15:11:40 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="left">
+        <img className="logo" src="./image 2.png" alt="" />
+        <h1 className="medx">Med-X</h1>
+      </div>
+      <div className="right">
+        <button className="about_us">About Us</button>
+        <button className="Register">Doctors</button>
+        <button className="connect_wallet">Connect Wallet <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png" className="wallet_pic" alt="" /></button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
