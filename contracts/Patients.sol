7d3b84bc4469d5e0cb4725f2a83a41954eb727e8
Sameer Madhav D <sameermadhav.5@gmail.com>
2023-01-08 08:11:23 +0530

    added patient smart contract

---
+++ contracts/Patients.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+//contract...
+contract Patients {
+    //Type Declaration
+    //struct
+    struct Patient {
+        address patientAddr;
+        string patientName;
+        uint256 age;
+        uint256 height;
+        string mobNo;
+    }
+
+    // Variables
+    mapping(address => Patient) public patients;
+
+    //functions
+    function addPatients(
+        /* function to add patients */
+        address _patientAddr,
+        string memory _patientName,
+        uint256 _age,
+        uint256 _height,
+        string memory _mobNo
+    ) public {
+        patients[_patientAddr] = Patient(
+            _patientAddr,
+            _patientName,
+            _age,
+            _height,
+            _mobNo
+        );
+    }
+
+    //Get Functions
+    function getPatientName(address _patientAddr)
+        public
+        view
+        returns (string memory)
+    {
+        return patients[_patientAddr].patientName;
+    }
+
+    function getAge(address _patientAddr) public view returns (uint256) {
+        return patients[_patientAddr].age;
+    }
+
+    function getHeight(address _patientAddr) public view returns (uint256) {
+        return patients[_patientAddr].height;
+    }
+
+    function getMobNo(address _patientAddr)
+        public
+        view
+        returns (string memory)
+    {
+        return patients[_patientAddr].mobNo;
+    }
+}
