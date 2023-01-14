0b9287d31bf9074f8f1410d39b2afddf6b25363e
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 20:13:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import './Navbar.css';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="Navbar">
       <div className="left">
-        <img className="logo" src="./Med-x-1 1.png" alt="" />
+        <Link to="/">
+          <img className="logo" src="./Med-x-1 1.png" alt="" />
+        </Link>
       </div>
       <div className="right">
-        <button className="about_us">About Us</button>
-        <button className="Register">Doctors</button>
-        <button className="appointments">Appointments</button>
-        <button className="connect_wallet">Connect Wallet <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png" className="wallet_pic" alt="" /></button>
+        <Link to="/aboutus">
+          <button className="about_us">About Us</button>
+        </Link>
+        <Link to="/doctors">
+          <button className="Register">Doctors</button>
+        </Link>
+        <Link to="/appointments">
+          <button className="appointments">Appointments</button>
+        </Link>
+        <button className="connect_wallet">
+          Connect Wallet{" "}
+          <Link to="/userprofile">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png"
+              className="wallet_pic"
+              alt=""
+            />
+          </Link>
+        </button>
       </div>
     </div>
   );
