3dc37ea38af7b64d620a84323fe060402e24f9e8
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 11:21:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Landingpage from './pages/Landingpage';
-import UserProfile from './pages/UserProfile';
+// import Landingpage from './pages/Landingpage';
+// import UserProfile from './pages/UserProfile';
 import Doctors from './pages/Doctors';
 
 function App() {
