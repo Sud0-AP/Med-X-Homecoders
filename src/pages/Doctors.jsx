84e231d895c7d10d807ba76ccbcf93cca9fa3e98
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 20:02:02 +0530

    Update Doctors.jsx

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import "./Doctors.css";
-import Navbar from "../components/Navbar";
 import DoctorCard from "../components/DoctorCard";
+import { Link } from "react-router-dom";
 
 function Doctors() {
   return (
     <div className="doctors">
-      <Navbar />
       <div className="doctors_header">
-        <button>back</button>
+      <Link to="/"><button>back</button></Link>
         <h1>OUR DOCTORS</h1>
       </div>
       <div className="doctor_cards_content">
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
-        <DoctorCard />
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
+      <Link to="/bookappointment"><DoctorCard /></Link>
       </div>
     </div>
   );
