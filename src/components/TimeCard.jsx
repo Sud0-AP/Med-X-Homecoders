84b5129e9dbccf540e532e98bd0f4309ce7b9f3a
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 14:02:07 +0530

    Add files via upload

---
+++ src/components/TimeCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TimeCard.css'
+
+function TimeCard() {
+  return (
+    <div className="time_card">
+        <p className="time_num">09:00</p>
+        <p className="time_period">AM</p>
+    </div>
+  )
+}
+
+export default TimeCard
