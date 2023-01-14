d823d18b245423e5fd17d8fdf4716eba085ad378
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 11:17:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Landingpage from './pages/Landingpage';
+import UserProfile from './pages/UserProfile';
+import Doctors from './pages/Doctors';
 
 function App() {
   return (
     <div className="App">
-      <Landingpage/>
+      {/* <Landingpage/> */}
+      {/* <UserProfile/> */}
+      <Doctors/>
     </div>
   );
 }
