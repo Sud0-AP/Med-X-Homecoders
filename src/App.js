67b9dedaf454bdf791e1b1f9a659e9c5f6e8edd5
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-14 19:54:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-// import Landingpage from './pages/Landingpage';
-// import UserProfile from './pages/UserProfile';
+import Landingpage from './pages/Landingpage';
+import UserProfile from './pages/UserProfile';
+import Navbar from './components/Navbar';
 import Doctors from './pages/Doctors';
+import BookAppointment from './pages/BookAppointment';
+import AboutUs from './pages/AboutUs';
+import Appointments from './pages/Appointments';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Landingpage/> */}
-      {/* <UserProfile/> */}
-      <Doctors/>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landingpage/>} />
+        <Route path='doctors' element={<Doctors/>} />
+        <Route path='aboutus' element={<AboutUs/>} />
+        <Route path='userprofile' element={<UserProfile/>} />
+        <Route path='appointments' element={<Appointments/>} />
+        <Route path='userprofile' element={<UserProfile/>} />
+        <Route path='bookappointment' element={<BookAppointment/>} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
