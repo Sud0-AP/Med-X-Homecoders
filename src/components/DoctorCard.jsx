f9e06538ca05c10113516dfebd37b2f13c9d29c0
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-16 23:46:16 +0530

    Update DoctorCard.jsx

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./DoctorCard.css";
 
-function DoctorCard() {
+function DoctorCard(props) {
   return (
     <div>
       <div className="doctor_card">
         <div className="doctor_image">
           <img
-            src="https://myupchar-banner.s3.ap-south-1.amazonaws.com/widget/avatar/doctor-avatar-male.png"
+            src={props.img}
             alt=""
           />
         </div>
         <div className="doctor_details">
           <div className="doctor_name">
-            <p>Tushar Sharma</p>
+            <p>{props.name}</p>
           </div>
           <div className="doctor_specialization">
-            <p>Cardiologist</p>
+            <p>{props.spec}</p>
           </div>
         </div>
         <div className="doctor_more_info">
-          <p className="location">📍 1.5 km away</p>
-          <p className="doctor_rating">⭐ 4.2</p>
+          <p className="location">📍 {props.dist} km away</p>
+          <p className="doctor_rating">⭐ {props.rate}</p>
         </div>
       </div>
     </div>
