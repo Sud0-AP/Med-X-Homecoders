cc2692bba5376966c64b65dcc97b45182f8a7305
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-16 23:47:25 +0530

    Update TimeCard.jsx

--- src/components/TimeCard.jsx
+++ src/components/TimeCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './TimeCard.css'
 
-function TimeCard() {
+function TimeCard(props) {
   return (
     <div className="time_card">
-        <p className="time_num">09:00</p>
+        <p className="time_num">{props.time}</p>
         <p className="time_period">AM</p>
     </div>
   )
