ac8284ceb9afb0c408f764d00eac470345972100
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-17 00:17:09 +0530

    Update RecentAppointment.jsx

--- src/components/RecentAppointment.jsx
+++ src/components/RecentAppointment.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './RecentAppointment.css'
 
-function RecentAppointment() {
+function RecentAppointment(props) {
   return (
     <div className="recent_appointments">
         <div className="left_content">
-            <p className="name">Tushar Sharma</p>
-            <p className="specialization">Cardiologist</p>
-            <p className="distance">📍 1.5 km away</p>
+            <p className="name">{props.name}</p>
+            <p className="specialization">{props.spec}</p>
+            <p className="distance">📍 {props.dist} km away</p>
         </div>
         <div className="right_content">
             <p className="your_rating">Your Rating:</p>
-            <p className="stars">⭐⭐⭐⭐</p>
+            <p className="stars">{props.rate}</p>
         </div>
     </div>
   )
