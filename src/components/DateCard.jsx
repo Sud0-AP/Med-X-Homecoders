763a0da171770f315bb2359bdc411e5959574b90
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-16 23:45:35 +0530

    Update DateCard.jsx

--- src/components/DateCard.jsx
+++ src/components/DateCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './DateCard.css'
 
-function DateCard() {
+function DateCard(props) {
   return (
     <div className="date_card">
-        <p className="date">10</p>
+        <p className="date">{props.date}</p>
         <p className="month">Dec</p>
     </div>
   )
